08ad133cdd6f92ca721d48c150c744be6effa764
Italo Mandara <italomandara@gmail.com>
2026-02-26 19:48:13 +0100

    remove commented out code

--- Procyon/Util/VDFTools.swift
+++ Procyon/Util/VDFTools.swift
@@ -9,22 +9,6 @@
 internal import Foundation
 
 func parseACFToDict(from file: String) -> [String:Any] {
-    /**
-     Incomplete shallow parser that the skips main property
-     parses an ACF file content String into a dictionary
-     */
-//    var dictionary: [String:String] = [:]
-//    let search1: Regex = /(("\w+?")\n\{\n(.*?\n)+\})+/
-//    let search2: Regex = /(\t"(\w+?)"\t+"(.*?)")\n(?=\t"\w+")/
-//    
-//    let matches = from.matches(of: search1)
-//    for match in matches {
-//        let values = match.0.matches(of: search2)
-//        for value in values {
-//            dictionary[value.2.description] = value.3.description
-//        }
-//    }
-//    return dictionary
     return parseVDFToDict(from: file)
 }
 

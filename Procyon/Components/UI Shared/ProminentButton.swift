484950480f2d0811204784bd108883bf022c6350
Italo Mandara <italomandara@gmail.com>
2026-03-21 21:29:50 +0100

    More layout improvements, size optimizations

---
+++ Procyon/Components/UI Shared/ProminentButton.swift
@@ -0,0 +1,18 @@
+//
+//  ProminentButton.swift
+//  Procyon
+//
+//  Created by Italo Mandara on 21/03/2026.
+//
+
+import SwiftUI
+
+struct ProminentButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProminentButton()
+}
